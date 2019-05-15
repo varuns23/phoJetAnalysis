94005a89e50d0b363624839f9c25a6a7aab8345e
Varun Sharma <varun.sharma@cern.ch>
2019-05-15 14:44:36 -0500

    added confgig and multicrab for monoHiggs

--- phoJetNtuplizer/test/crab/multicrab-2018-data_monoHiggs.py
+++ phoJetNtuplizer/test/crab/multicrab-2018-data_monoHiggs.py
@@ -1,11 +1,15 @@
 import os
 
 dataset = {
-  'MET2018Dv1' : '/MET/Run2018D-PromptReco-v1/MINIAOD',
-  'MET2018Dv2' : '/MET/Run2018D-PromptReco-v2/MINIAOD',
 
-  'EGamma2018Dv1' : '/EGamma/Run2018D-PromptReco-v1/MINIAOD',
-  'EGamma2018Dv2' : '/EGamma/Run2018D-PromptReco-v2/MINIAOD',
+'sMu2018A': '/SingleMuon/Run2018A-17Sep2018-v2/MINIAOD',
+'sMu2018B': '/SingleMuon/Run2018B-17Sep2018-v1/MINIAOD',
+'sMu2018C': '/SingleMuon/Run2018C-17Sep2018-v1/MINIAOD',
+
+'Tau2018A': '/Tau/Run2018A-17Sep2018-v1/MINIAOD',
+'Tau2018B': '/Tau/Run2018B-17Sep2018-v1/MINIAOD',
+'Tau2018C': '/Tau/Run2018C-17Sep2018-v1/MINIAOD',
+
 }
 
 if __name__ == '__main__':
@@ -16,13 +20,14 @@ def submit(config):
 
 from CRABClient.UserUtilities import config
 config = config()
-name = 'data2018_Prompt'
+name = 'data2018_17Sep2018_monoHiggs'
 config.General.workArea = 'crab_'+name
 config.General.transferOutputs = True
 config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
-#config.JobType.inputFiles = ['inputFiles']
+config.JobType.inputFiles = ['Autumn18_RunABCD_V8_DATA.db']
+config.JobType.allowUndistributedCMSSW = True
 
 config.section_('Data') 
 config.Data.publication = False
@@ -34,24 +39,24 @@ config.Site.storageSite = 'T2_US_Wisconsin'
 #config.Site.whitelist = ["T2_US_Wisconsin"]
 #config.Site.blacklist = ['T2_CH_CERN']
 
-#listOfSamples = ['MET2018Bv1', 'MET2018Bv2', 'MET2018Dv1', 'MET2018Dv2']
-listOfSamples = ['EGamma2018Dv1', 'EGamma2018Dv2']
+#listOfSamples = ['MET2018A', 'MET2018B', 'MET2018C']
+listOfSamples = ['Tau2018A']
 
 
 for sample in listOfSamples:  
-  os.popen('cp run_102X_data2018.py run_102X_data2018_'+sample+'.py')
-  with open('run_102X_data2018_'+sample+'.py') as oldFile:
+  os.popen('cp run_102X_data2018_monoHiggs.py run_102X_data2018_monoHiggs_'+sample+'.py')
+  with open('run_102X_data2018_monoHiggs_'+sample+'.py') as oldFile:
     newText = oldFile.read().replace('Ntuple_data2018.root','Data_'+sample+'.root')
-  with open('run_102X_data2018_'+sample+'.py', 'w') as newFile:
+  with open('run_102X_data2018_monoHiggs_'+sample+'.py', 'w') as newFile:
     newFile.write(newText)
 
   config.General.requestName = 'job_'+sample
   
-  config.JobType.psetName = 'run_102X_data2018_'+sample+'.py'
+  config.JobType.psetName = 'run_102X_data2018_monoHiggs_'+sample+'.py'
   config.JobType.outputFiles = ['Data_'+sample+'.root']
   
   config.Data.inputDataset   = dataset[sample]
-  config.Data.unitsPerJob = 6
+  config.Data.unitsPerJob = 7
   config.Data.totalUnits = -1
   config.Data.outLFNDirBase = '/store/user/varuns/'+name
   submit(config)
