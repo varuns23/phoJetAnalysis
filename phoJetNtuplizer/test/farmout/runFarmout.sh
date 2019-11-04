0ee9c1d79731b938b14d7452d68831e6c1161390
Varun Sharma <varun.sharma@cern.ch>
2019-11-03 22:48:58 -0600

    updated farmout scripts

--- phoJetNtuplizer/test/farmout/runFarmout.sh
+++ phoJetNtuplizer/test/farmout/runFarmout.sh
@@ -5,23 +5,24 @@ datasets=( /EGamma/Run2018D-22Jan2019-v2/MINIAOD
     )
 
 dirName=(
-    Egamma2018D-22Jan2019-Test2
+    Egamma2018D
     )
 
-#dataset="/MET/Run2017B-31Mar2018-v1/MINIAOD"
 
 index=-1;
 for sampleName in "${datasets[0]}"; do
   index=$(( $index+1 ))
 
-  echo submitting jobs for "$sampleName"  with out dir name "${dirName[$index]}"
+ echo submitting jobs for "$sampleName"  with out dir name "${dirName[$index]}"
 
 farmoutAnalysisJobs \
- --job-count=5  \
- --input-files-per-job=1 \
+ --skip-existing-output  --input-files-per-job=1 \
  --lumi-mask=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/ReReco/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt \
  --input-dbs-path=$sampleName \
  --extra-inputs=Autumn18_RunABCD_V19_DATA.db \
+ --extra-usercode-files=cfipython \
+ --memory-requirements=4000 \
+ --base-requirements='TARGET.PoolName == "HEP" && ((MY.RequiresSharedFS =!= true || TARGET.HasAFS_OSG) && (TARGET.OSG_major =!= undefined || TARGET.IS_GLIDEIN =?= true) && (TARGET.HasParrotCVMFS =?= true || (TARGET.UWCMS_CVMFS_Exists && TARGET.CMS_CVMFS_Exists && TARGET.UWCMS_CVMFS_Revision >= 444 && TARGET.CMS_CVMFS_Revision >= 81620))) && ((MY.NoAutoRequirements =?= true || (TARGET.OSglibc_major == 2 && TARGET.OSglibc_minor >= 17 && (MY.HEP_VO =!= "uscms" || TARGET.CMS_CVMFS_Exists && TARGET.UWCMS_CVMFS_Exists)))) && (TARGET.Arch == "X86_64") && (TARGET.OpSys == "LINUX") && (TARGET.Disk >= RequestDisk) && (TARGET.Memory >= RequestMemory) && (TARGET.HasFileTransfer)' \
  ${dirName[$index]} \
  /cms/varuns/2018/CMSSW_10_2_10 \
  /cms/varuns/2018/CMSSW_10_2_10/src/phoJetAnalysis/phoJetNtuplizer/test/run_102X_data2018_farmout.py
